47dc2ffa767f613434f86611f058b4518f371b92
Brittanie Barrett <britt.barrett302@gmail.com>
2020-08-08 14:03:30 -0700

    edits to searchform file

---
+++ src/components/SearchForm/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./style.css";
+
+function SearchForm(props) {
+  return (
+    <form className="search">
+      <div className="form-group">
+        <label htmlFor="employee">Employee</label>
+        <input
+          value={props.search}
+          onChange={props.handleInputChange}
+          name="employee"
+          list="employees"
+          type="text"
+          className="form-control"
+          placeholder="Begin typing employee name you wish you locate here."
+          id="employee"
+        />
+        <datalist id="employees">
+          {props.employees.map((employee) => (
+            <option value={employee} key={employee} />
+          ))}
+        </datalist>
+        <button
+          type="submit"
+          onClick={props.handleFormSubmit}
+          className="btn btn-success"
+        >
+          Search
+        </button>
+      </div>
+    </form>
+  );
+}
+
+export default SearchForm;
